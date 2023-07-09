c4c86cfebaad10acfc7c93c6f8aee69087bc9937
AlirezaGha11 <138559333+AlirezaGha11@users.noreply.github.com>
2023-07-09 22:12:12 +0330

    Add files via upload

---
+++ import math.py
@@ -0,0 +1,8 @@
+import math
+a=int(input("ENTER THE FIRST NUMBER:"))
+b=int(input("ENTER THE SECOND NUMBER:"))
+c=a+b
+print(math.sin(math.radians(c)))
+print(math.cos(math.radians(c)))
+print(math.tan(math.radians(c)))
+print(math.factori
